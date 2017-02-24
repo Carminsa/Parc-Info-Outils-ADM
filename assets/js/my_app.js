3fc45612fe5d08818d45a7e2106d820aea471936
geo <geoffrey.leborges@epitech.eu>
2017-02-24 11:15:06 +0100

    span for other choice in select

---
+++ assets/js/my_app.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    $('#span_more_garant').on('click', function(){
+        $('#input_more_garant').addClass('active');
+    });
+
+    $('#span_more_os').on('click', function(){
+        $('#input_more_os').addClass('active');
+    });
+
+    $('#span_more_state').on('click', function(){
+        $('#input_more_state').addClass('active');
+    });
+
+    $('#span_more_user').on('click', function(){
+        $('#input_more_user').addClass('active');
+    });
+});
